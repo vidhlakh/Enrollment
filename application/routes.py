9f538ef260a4f6b5fc0cf3996903b9a88533c67d
Vidhya Lakshmi Sankaranarayanan <vidhlakh@gmail.com>
2020-04-29 19:54:02 -0400

    Update index.html, footer.html, nav.html, routes.py

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,20 @@
 from application import app
+from flask import render_template
+
 @app.route("/")
 @app.route("/index")
+@app.route("/home")
 def index():
-    return "<h1> Hello Earth! </h1>"
+    return render_template("index.html", login=False)
+
+@app.route("/login")
+def login():
+    return render_template("login.html", login=False)
+
+@app.route("/courses")
+def courses():
+    return render_template("course.html", login=False)
+
+@app.route("/register")
+def register():
+    return render_template("register.html", login=False)
